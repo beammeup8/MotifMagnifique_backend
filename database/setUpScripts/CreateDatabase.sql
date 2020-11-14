bd4d37e0eeaeb0949ee66f4ec08ba72021566f40
Miranda <flowerbutterfly8@gmail.com>
2020-11-14 14:34:08 -0500

    added a user with admin priviledges

--- database/setUpScripts/CreateDatabase.sql
+++ database/setUpScripts/CreateDatabase.sql
@@ -2,3 +2,7 @@ DROP Database IF Exists glowstick;
 
 Create Database glowstick;
 Use glowstick;
+
+
+Drop USER IF Exists user;
+GRANT ALL PRIVILEGES ON *.* TO user@localhost IDENTIFIED BY 'password';
