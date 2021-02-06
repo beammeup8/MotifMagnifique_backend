7e37d2828b7e49680707cd9a655c2f79639785a8
Miranda <flowerbutterfly8@gmail.com>
2021-02-06 14:28:24 -0500

    now runs all the scripts that we have so far

--- database/runAllSQL.bash
+++ database/runAllSQL.bash
@@ -1,13 +1,22 @@
 #!/bin/bash
+pwd
 initialCreationScript=InitialDatabase.sql
 mysql -u root -p motifMagnifique < database/setUpScripts/$initialCreationScript
-echo 'Created initial database'
+echo 'Ran the initial database script'
 fileContents='sudo cat database/userLoginInfo.yaml'
 username=$($fileContents | shyaml get-value username)
 password=$($fileContents | shyaml get-value password)
 
-for filename in Tables.sql CreateStoredProcedures.sql
-do
+filesToRun=(
+  Tables.sql
+  CreateStoredProcedures.sql
+  ReadStoredProcedures.sql
+  UpdateStoredProcedures.sql
+  DeleteStoredProcedures.sql
+  )
+
+
+for filename in ${filesToRun[@]};do
   echo 'Running' $filename
   mysql -u ${username} -ppassword motifMagnifique < database/setUpScripts/$filename
 done
