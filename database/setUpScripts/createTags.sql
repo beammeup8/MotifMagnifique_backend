472e096478ac5e3799d3e00a9a3fc6d51b585455
Mike <mike@pop-os.localdomain>
2021-03-07 18:43:40 -0500

    pushing pre-made code before switching branches

--- database/setUpScripts/createTags.sql
+++ database/setUpScripts/createTags.sql
@@ -10,10 +10,26 @@ DELIMITER //
   BEGIN
     INSERT INTO tag(name, value) 
     VALUES (t_name, t_value);
-  
+
+    SELECT id FROM tag
+    WHERE name = t_name;
   END 
 //    
 
+Create Fuction AddTag
+( t_name VARCHAR(50),
+  t_value VARCHAR(50) )
+  
+RETURNS bigint(50)
+
+BEGIN 
+SELECT name FROM tag
+  WHERE name = t_name
+  IF FIRST(name) is NULL
+
+
+
+//
 Create Procedure 
 CreateUnit
 (u_name               VARCHAR(20),
@@ -30,5 +46,3 @@ BEGIN
 END 
 
 // 
-
-  
