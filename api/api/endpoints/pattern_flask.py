ccb7365f5633a02e9d56083e7d15e062ae7d0643
Miranda <flowerbutterfly8@gmail.com>
2021-04-04 22:48:40 -0400

    added optional fields

--- api/api/endpoints/pattern_flask.py
+++ api/api/endpoints/pattern_flask.py
@@ -8,9 +8,29 @@ from .response_gen import lst_tuple_response, dict_response
 
 def construct_blueprint(database, userConn):
     pattern = Blueprint('pattern', __name__)
-    
+
     @pattern.route('/details', methods=['GET'])
-    def get_user_details():
+    def get_pattern_details():
+        field_names = ['username', 'authtoken', 'patternName']
+        is_valid, validated_fields = validate_params(request.args, field_names)
+        if not is_valid:
+            return lst_tuple_response(validated_fields, BAD_REQUEST)
+        return {}
+
+    @pattern.route('/new', methods=['PUT'])
+    def create_pattern():
+        field_names = ['username', 'authtoken', 'patternName']
+        is_valid, validated_fields = validate_params(request.args, field_names)
+        if not is_valid:
+            return lst_tuple_response(validated_fields, BAD_REQUEST)
+        return {}
+
+    @pattern.route('/edit', methods=['PUT'])
+    def edit_pattern():
+        field_names = ['username', 'authtoken', 'patternName']
+        is_valid, validated_fields = validate_params(request.args, field_names)
+        if not is_valid:
+            return lst_tuple_response(validated_fields, BAD_REQUEST)
         return {}
 
     return pattern
