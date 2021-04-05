cb722e35f2a0d162e2b770f0d2ce0183bfe5e81c
Miranda <flowerbutterfly8@gmail.com>
2021-04-04 22:37:29 -0400

    some cleanup

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_httpauth import HTTPTokenAuth
 
-from .endpoints.user_flask import construct_blueprint
+from .endpoints.user_flask import construct_blueprint as make_user_blueprint
+from .endpoints.pattern_flask import construct_blueprint as make_pattern_blueprint
 from .database.Database import Database
 from .database.accessors.UserConnector import UserConnector
 
@@ -12,5 +13,8 @@ database = Database('api/api/database/userLoginInfo.yaml')
 
 userConn = UserConnector(database)
 
-user_flask = construct_blueprint(database, userConn, auth)
+user_flask = make_user_blueprint(database, userConn, auth)
 app.register_blueprint(user_flask, url_prefix='/user')
+
+pattern_flask = make_pattern_blueprint(database, userConn)
+app.register_blueprint(pattern_flask, url_prefix='/pattern')
